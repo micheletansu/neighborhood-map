5e02ed78767bf100446664f25d57b81a3b5d2262
micheletansu <micheletansu@hotmail.com>
2015-02-26 09:23:17 +0100

    altervista

--- js/app.js
+++ js/app.js
@@ -1,11 +1,72 @@
+var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 5,
+    center: new google.maps.LatLng(55, 11),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
 function AppViewModel() {
     var self = this;
-    this.location = ko.observable("Aggius");
-    //this.mapUrl = ko.observable('https://www.google.com/maps/embed/v1/search?q=aggius&key=AIzaSyBVAy0aVHbQvQ6NRCQGAOBCFVSMzJOouYA');
+    self.location = ko.observable("Aggius");
+    self.quotaUser = ko.observable('quotaUser=mich');
+    self.mapKey = ko.observable('&key=AIzaSyBVAy0aVHbQvQ6NRCQGAOBCFVSMzJOouYA');
+    self.$canvasMap = document.getElementById('map-canvas');
+    //self.map = {};
 
-    this.mapUrl = ko.computed(function() {
-        return 'https://www.google.com/maps/embed/v1/search?q=' + this.location() + '&key=AIzaSyBVAy0aVHbQvQ6NRCQGAOBCFVSMzJOouYA';
+    self.mapUrl = ko.computed(function() {
+        return 'https://www.google.com/maps/embed/v3/search?q=' + this.location() + this.mapKey();
     }, this);
+
+    self.addMap = function(d, e) {
+        if (e.keyCode==13) { //ENTER_KEY
+            var mapOptions = {
+              center: { lat: -34.397, lng: 150.644},
+              zoom: 8
+            };
+            //this.map = new google.maps.Map(this.$canvasMap, mapOptions);
+            map = new google.maps.Map(this.$canvasMap, mapOptions);
+        }
+    }
 }
- 
+/*
+ko.bindingHandlers.map = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var mapObj = ko.utils.unwrapObservable(valueAccessor());
+        var latLng = new google.maps.LatLng(
+            ko.utils.unwrapObservable(mapObj.lat),
+            ko.utils.unwrapObservable(mapObj.lng));
+        var mapOptions = { center: latLng,
+                          zoom: 6, 
+                          mapTypeId: google.maps.MapTypeId.ROADMAP};
+
+        mapObj.googleMap = new google.maps.Map(element, mapOptions);
+
+        mapObj.marker = new google.maps.Marker({
+            map: mapObj.googleMap,
+            position: latLng,
+            title: "You Are Here",
+            draggable: true
+        });     
+
+        mapObj.onChangedCoord = function(newValue) {
+            var latLng = new google.maps.LatLng(
+                ko.utils.unwrapObservable(mapObj.lat),
+                ko.utils.unwrapObservable(mapObj.lng));
+            mapObj.googleMap.setCenter(latLng);                 
+        };
+
+        mapObj.onMarkerMoved = function(dragEnd) {
+            var latLng = mapObj.marker.getPosition();
+            mapObj.lat(latLng.lat());
+            mapObj.lng(latLng.lng());
+        };
+
+        mapObj.lat.subscribe(mapObj.onChangedCoord);
+        mapObj.lng.subscribe(mapObj.onChangedCoord);  
+
+        google.maps.event.addListener(mapObj.marker, 'dragend', mapObj.onMarkerMoved);
+
+        $("#" + element.getAttribute("id")).data("mapObj",mapObj);
+    }
+};
+*/
 ko.applyBindings(new AppViewModel());
