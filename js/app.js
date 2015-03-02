d09acb9d425593fda675d34463bf987725070ecd
micheletansu <micheletansu@hotmail.com>
2015-03-02 17:02:26 +0100

    new box container for map and info

--- js/app.js
+++ js/app.js
@@ -23,10 +23,23 @@ function AppViewModel() {
 
 	// Aggiunge una mappa con un maker relativa alla mappa nella sezione principale
     self.addMap = function(d, e) {
+      var $boxDiv = document.createElement('div');
+      $boxDiv.className = 'box-sel';
+      self.$leftBar.appendChild($boxDiv);
+
       var $selectedMapDiv = document.createElement('div');
       $selectedMapDiv.id = 'sel-map'+self.selectedMaps().length;
       $selectedMapDiv.className = 'sel-map';
-      self.$leftBar.appendChild($selectedMapDiv);
+      $boxDiv.appendChild($selectedMapDiv);
+
+      var $selectedInfoDiv = document.createElement('div');
+      $selectedInfoDiv.className = 'sel-info';
+      $selectedInfoDiv.textContent = 'oooooooooooooooo';
+      $boxDiv.appendChild($selectedInfoDiv);
+
+      var $clearDiv = document.createElement('div');
+      $clearDiv.className = 'clear-div';
+      $boxDiv.appendChild($clearDiv);
         
       self.geocoder.geocode( { 'address': this.location() }, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
